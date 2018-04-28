6eb62d8b09b24e191257e3749f89272254c500bd
Andrew Yang <andrewyang@AndrewdeMacBook-Pro.local>
2018-04-29 00:30:39 +0800

    更新

---
+++ src/components/common/bodyNav.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="body-nav fx bd-b">
+    <a class="fx-1 fx fx-justify-center" href="#">
+      <div>
+        <div class="icon-box fx fx-justify-center fx-align-center">
+          <i class="nav-icon iconfont icon-tabhome"></i>
+        </div>
+        <div class="nav-txt">新房楼盘</div>
+      </div>
+    </a>
+    <a class="fx-1 fx fx-justify-center" href="#">
+      <div>
+        <div class="icon-box fx fx-justify-center fx-align-center">
+          <i class="nav-icon iconfont icon-tabhome"></i>
+        </div>
+        <div class="nav-txt">新房楼盘</div>
+      </div>
+    </a>
+    <a class="fx-1 fx fx-justify-center" href="#">
+      <div>
+        <div class="icon-box fx fx-justify-center fx-align-center">
+          <i class="nav-icon iconfont icon-tabhome"></i>
+        </div>
+        <div class="nav-txt">新房楼盘</div>
+      </div>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+.body-nav {
+  padding: 15px 0;
+  background: #fff;
+  .icon-box {
+    width: 48px;
+    height: 48px;
+    margin: auto;
+    border-radius: 50%;
+    background: #e6b322;
+    .nav-icon {
+      width: 24px;
+      height: 24px;
+      font-size: 24px;
+      color: #fff;
+      line-height: 1;
+      overflow: hidden;
+    }
+    .nav-txt {
+      font-size: 14px;
+      color: #333;
+      line-height: 1.2;
+      margin-top: 5px;
+      text-align: center;
+    }
+  }
+}
+</style>
+
+
--- src/components/common/headNav.vue
+++ src/components/common/headNav.vue
@@ -1,19 +1,42 @@
 <template>
-  <section class="head-nav">
-
+  <section class="head-nav fx fx-justify-between fx-align-center">
+    <a class="go-back" :style="goBackStyle" href="javascript:history.back();"></a>
+    <img :src="logoSrc" height="30" alt="">
+    <i class="icon iconfont icon-caidan menu"></i>
   </section>
 </template>
 <script>
+import logo from '@/assets/imgs/logo.jpg'
+import sprite_newh from '@/assets/imgs/sprite_newh.svg'
 export default {
+  data () {
+    return {
+      logoSrc: logo,
+      goBackStyle: {
+        background: `url(${sprite_newh}) no-repeat`,
+        backgroundSize: 'cover',
+        backgroundPosition: '0 -407px'
+      }
+    }
+  }
   
 }
 </script>
 <style lang="scss" scoped>
   .head-nav {
-    width: 100%;
     height: 50px;
     background: #fff;
     padding: 0 19px;
+    .go-back {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+    }
+    .menu {
+      color: #00ae66;
+      font-size: 16px;
+      line-height: 1;
+    }
   }
 </style>
 
